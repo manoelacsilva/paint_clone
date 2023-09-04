88092a1d498a345e52fb7925931888bcd93d8618
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-09-04 20:39:52 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("canvas");
+const contexto = canvas.getContext("2d");
+
+const inputColor = document.querySelector(".input__color");
+const tools = document.querySelectorAll(".button__tool");
+const sizeButtons = document.querySelector(".button__size");
+const buttonClear = document.querySelector(".button__clear");
