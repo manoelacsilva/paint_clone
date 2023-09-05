8d5cf3240f22a0eeeae562815007f423bba7a973
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-09-04 23:08:50 -0300

    trabalhando na brush

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ const inputColor = document.querySelector(".input__color");
 const tools = document.querySelectorAll(".button__tool");
 const sizeButtons = document.querySelector(".button__size");
 const buttonClear = document.querySelector(".button__clear");
+
+let brushSize = 10;
+
+contexto.fillStyle = "#000"
+
+let isPainting = false
+
+canvas.addEventListener("mousedown", ({clientX, clientY}) => {
+    draw(clientX, clientY)
+    isPainting = true
+})
+
+canvas.addEventListener("mousemove", ({clientX, clientY}) => {
+    if (isPainting) {
+        draw(clientX, clientY)
+    }
+})
+
+canvas.addEventListener("mouseup", ({clientX, clientY}) => {
+    isPainting = false
+})
+
+const draw = (x, y) => {
+    contexto.beginPath()
+    contexto.arc (
+        x - canvas.offsetLeft,
+        y - canvas.offsetTop,
+        brushSize / 2,
+        0,
+        2 * Math.PI
+    )
+    contexto.fill()
+}
